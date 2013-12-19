7f2cc4dccceb3b6d1bf17beb3d310f8c49ccaece
Mike Ortiz <mortiz@cs.stanford.edu>
2013-12-19 01:02:15 -0800

    Removing onClickListener from example activity

--- src/com/example/touch/TouchImageViewActivity.java
+++ src/com/example/touch/TouchImageViewActivity.java
@@ -2,27 +2,15 @@ package com.example.touch;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
 
 
 public class TouchImageViewActivity extends Activity {
-	
-	TouchImageView img;
-	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        img = (TouchImageView) findViewById(R.id.img);
+        TouchImageView img = (TouchImageView) findViewById(R.id.img);
         img.setMaxZoom(4);
-        img.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				img.setImageResource(R.drawable.icon);
-			}
-		});
     }
 }
