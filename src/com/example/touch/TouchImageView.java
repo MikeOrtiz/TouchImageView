5009a00ba252cc58d76a83ae93cf82ef5abf1000
Mike Ortiz <mortiz@cs.stanford.edu>
2014-04-14 19:30:32 -0700

    Bug fix so that stretching image to super min and max will be mirrored correctly.

--- src/com/example/touch/TouchImageView.java
+++ src/com/example/touch/TouchImageView.java
@@ -380,7 +380,7 @@ public class TouchImageView extends ImageView {
     	
     	setScaleType(scaleType);
     	resetZoom();
-    	scaleImage(scale, viewWidth / 2, viewHeight / 2, false);
+    	scaleImage(scale, viewWidth / 2, viewHeight / 2, true);
     	matrix.getValues(m);
     	m[Matrix.MTRANS_X] = -((focusX * getImageWidth()) - (viewWidth * 0.5f));
     	m[Matrix.MTRANS_Y] = -((focusY * getImageHeight()) - (viewHeight * 0.5f));
