5120a427a934dcd474728bc24f773317fd7130cc
Mike Ortiz <mortiz@cs.stanford.edu>
2014-04-17 09:47:54 -0700

    Added comment to ExtendedViewPager.

--- src/com/ortiz/touch/ExtendedViewPager.java
+++ src/com/ortiz/touch/ExtendedViewPager.java
@@ -19,7 +19,13 @@ public class ExtendedViewPager extends ViewPager {
 	@Override
 	protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
 	    if (v instanceof TouchImageView) {
+	    	//
+	    	// canScrollHorizontally is not supported for Api < 14. To get around this issue,
+	    	// ViewPager is extended and canScrollHorizontallyFroyo, a wrapper around
+	    	// canScrollHorizontally supporting Api >= 8, is called.
+	    	//
 	        return ((TouchImageView) v).canScrollHorizontallyFroyo(-dx);
+	        
 	    } else {
 	        return super.canScroll(v, checkV, dx, x, y);
 	    }
