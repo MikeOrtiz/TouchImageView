6045ba442919f213036d8208d5f4f63ae6b1523c
Hannes Achleitner <hannes.software@gmx.at>
2019-10-12 10:14:41 +0200

    zoom in RecyclerView

--- app/src/main/java/com/ortiz/touchdemo/AdapterImages.kt
+++ app/src/main/java/com/ortiz/touchdemo/AdapterImages.kt
@@ -1,5 +1,6 @@
 package com.ortiz.touchdemo
 
+import android.view.MotionEvent
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +23,30 @@ class AdapterImages(private val photoList: IntArray) : RecyclerView.Adapter<Adap
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(TouchImageView(parent.context).apply {
             layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
+            setOnTouchListener { view, event ->
+                var result = true
+                //can scroll horizontally checks if there's still a part of the image
+                //that can be scrolled until you reach the edge
+                if (event.pointerCount >= 2 || view.canScrollHorizontally(1) && canScrollHorizontally(-1)) {
+                    //multi-touch event
+                    result = when (event.action) {
+                        MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                            // Disallow RecyclerView to intercept touch events.
+                            parent.requestDisallowInterceptTouchEvent(true)
+                            // Disable touch on view
+                            false
+                        }
+                        MotionEvent.ACTION_UP -> {
+                            // Allow RecyclerView to intercept touch events.
+                            parent.requestDisallowInterceptTouchEvent(false)
+                            true
+                        }
+                        else -> true
+                    }
+                }
+                result
+            }
         })
     }
 
