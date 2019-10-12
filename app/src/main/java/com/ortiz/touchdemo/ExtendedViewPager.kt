15d1d8a90420b0c9bc41375b442c3b18667a063f
Hannes Achleitner <hannes.software@gmx.at>
2019-10-12 09:32:07 +0200

    remove some lints

--- app/src/main/java/com/ortiz/touchdemo/ExtendedViewPager.kt
+++ app/src/main/java/com/ortiz/touchdemo/ExtendedViewPager.kt
@@ -9,9 +9,9 @@ import com.ortiz.touchview.TouchImageView
 
 class ExtendedViewPager : ViewPager {
 
-    constructor(context: Context) : super(context) {}
+    constructor(context: Context) : super(context)
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
     override fun canScroll(view: View, checkV: Boolean, dx: Int, x: Int, y: Int): Boolean {
         return if (view is TouchImageView) {
