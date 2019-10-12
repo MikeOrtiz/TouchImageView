dd69b26200384df78bc0b15ef6f7e5c9e0deef03
Hannes Achleitner <hannes.software@gmx.at>
2019-10-12 19:02:55 +0200

    Save previous matrix values when zooming
    Save previous matrix values when zooming to get the correct matrix values from prevMatrix when the view is measured right after.
    Changes from https://github.com/MikeOrtiz/TouchImageView/pull/174

--- touchview/src/main/java/com/ortiz/touchview/TouchImageView.java
+++ touchview/src/main/java/com/ortiz/touchview/TouchImageView.java
@@ -501,6 +501,7 @@ public class TouchImageView extends AppCompatImageView {
         m[Matrix.MTRANS_Y] = -((focusY * getImageHeight()) - (viewHeight * 0.5f));
         matrix.setValues(m);
         fixTrans();
+        savePreviousImageValues();
         setImageMatrix(matrix);
     }
 
