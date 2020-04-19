4067c3978aacfb090e2be05ece7040d3c7312a1e
Hannes Achleitner <hannes.software@gmx.at>
2020-04-19 08:00:30 +0200

    ability of no auto fit

--- touchview/src/main/java/com/ortiz/touchview/TouchImageView.kt
+++ touchview/src/main/java/com/ortiz/touchview/TouchImageView.kt
@@ -160,6 +160,15 @@ class TouchImageView @JvmOverloads constructor(context: Context, attrs: Attribut
         fitImageToView()
     }
 
+    fun setImageDrawable(drawable: Drawable?, fitImage: Boolean) {
+        imageRenderedAtLeastOnce = false
+        super.setImageDrawable(drawable)
+        if (fitImage) {
+            savePreviousImageValues()
+            fitImageToView()
+        }
+    }
+
     override fun setImageDrawable(drawable: Drawable?) {
         imageRenderedAtLeastOnce = false
         super.setImageDrawable(drawable)
