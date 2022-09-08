d4609fc37d8ff0e306decdb66510c961312ec73b
Hannes Achleitner <hannes.software@gmx.at>
2022-09-08 07:48:45 +0200

    api33

--- touchview/src/main/java/com/ortiz/touchview/TouchImageView.kt
+++ touchview/src/main/java/com/ortiz/touchview/TouchImageView.kt
@@ -792,16 +792,16 @@ open class TouchImageView @JvmOverloads constructor(context: Context, attrs: Att
      * to the view's listener.
      */
     private inner class GestureListener : SimpleOnGestureListener() {
-        override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
+        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
             // Pass on to the OnDoubleTapListener if it is present, otherwise let the View handle the click.
             return doubleTapListener?.onSingleTapConfirmed(e) ?: performClick()
         }
 
-        override fun onLongPress(e: MotionEvent?) {
+        override fun onLongPress(e: MotionEvent) {
             performLongClick()
         }
 
-        override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+        override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
             // If a previous fling is still active, it should be cancelled so that two flings
             // are not run simultaneously.
             fling?.cancelFling()
@@ -809,9 +809,9 @@ open class TouchImageView @JvmOverloads constructor(context: Context, attrs: Att
             return super.onFling(e1, e2, velocityX, velocityY)
         }
 
-        override fun onDoubleTap(e: MotionEvent?): Boolean {
+        override fun onDoubleTap(e: MotionEvent): Boolean {
             var consumed = false
-            if (e != null && isZoomEnabled) {
+            if (isZoomEnabled) {
                 doubleTapListener?.let {
                     consumed = it.onDoubleTap(e)
                 }
@@ -826,7 +826,7 @@ open class TouchImageView @JvmOverloads constructor(context: Context, attrs: Att
             return consumed
         }
 
-        override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
+        override fun onDoubleTapEvent(e: MotionEvent): Boolean {
             return doubleTapListener?.onDoubleTapEvent(e) ?: false
         }
     }
