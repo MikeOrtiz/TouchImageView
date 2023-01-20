f95845e7df2692c729cd7d6095f437ca88f028e5
Hannes Achleitner <hannes.software@gmx.at>
2023-01-20 11:46:32 +0100

    Test for zoom

---
+++ app/src/androidTest/java/info/touchimage/demo/ZoomTest.kt
@@ -0,0 +1,47 @@
+package info.touchimage.demo
+
+import androidx.test.core.app.takeScreenshot
+import androidx.test.core.graphics.writeToTestStorage
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestName
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ZoomTest {
+
+    @get:Rule
+    val activityScenarioRule = activityScenarioRule<AnimateZoomActivity>()
+
+    @get:Rule
+    var nameRule = TestName()
+
+    @Test
+    fun zoom() {
+        Thread.sleep(WAIT)
+        takeScreenshot()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-1-init")
+        onView(withId(R.id.current_zoom)).perform(ViewActions.click())
+        Thread.sleep(WAIT)
+        takeScreenshot()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-2-reset")
+        onView(withId(R.id.current_zoom)).perform(ViewActions.click())
+        Thread.sleep(WAIT)
+        takeScreenshot()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-3-zoom")
+        onView(withId(R.id.current_zoom)).perform(ViewActions.click())
+        Thread.sleep(WAIT)
+        takeScreenshot()
+            .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-4-end")
+    }
+
+    companion object {
+        const val WAIT = 600L
+    }
+
+}
