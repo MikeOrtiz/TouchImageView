74f0c589097d50dda8a8f7ced779678a79ef48cc
Hannes Achleitner <hannes.software@gmx.at>
2023-01-22 11:12:34 +0100

    Reenable flaky test

--- app/src/androidTest/java/info/touchimage/demo/TouchTest.kt
+++ app/src/androidTest/java/info/touchimage/demo/TouchTest.kt
@@ -5,12 +5,11 @@ import androidx.test.core.graphics.writeToTestStorage
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.screenshot.captureToBitmap
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.moka.EspressoMoka
 import info.touchimage.demo.utils.MultiTouchDownEvent
 import info.touchimage.demo.utils.TouchAction
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestName
@@ -36,19 +35,17 @@ class TouchTest {
     }
 
     @Test
-    @Ignore("It is flaky")
     fun testMultiTouch() {
-        onView(ViewMatchers.isRoot())
-            .captureToBitmap()
+        EspressoMoka.onView(ViewMatchers.isRoot())
+        takeScreenshot()
             .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-before")
         val touchList: Array<Pair<Float, Float>> = listOf(
             Pair(4f, 8f),
             Pair(40f, 80f),
             Pair(30f, 70f)
         ).toTypedArray()
-        onView(withId(R.id.imageSingle)).perform(MultiTouchDownEvent(touchList))
-        onView(ViewMatchers.isRoot())
-            .captureToBitmap()
+        EspressoMoka.onView(withId(R.id.imageSingle)).perform(MultiTouchDownEvent(touchList))
+        takeScreenshot()
             .writeToTestStorage("${javaClass.simpleName}_${nameRule.methodName}-after")
     }
 }
