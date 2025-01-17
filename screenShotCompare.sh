ce02d720b00c13c9f6ebbe52d5a084379370cbd3
Hannes Achleitner <hannes.software@gmx.at>
2025-01-17 07:40:11 +0100

    screenshot compare as submodule

--- screenShotCompare.sh
+++
@@ -1,9 +0,0 @@
-diffFiles=./screenshotDiffs
-mkdir $diffFiles
-#cp app/build/outputs/connected_android_test_additional_output/debugAndroidTest/connected/emulator\(AVD\)\ -\ 9/* screenshotsToCompare
-set -x
-./git-diff-image/install.sh
-GIT_DIFF_IMAGE_OUTPUT_DIR=$diffFiles git diff-image
-
-# set error when diffs are there
-[ "$(ls -A $diffFiles)" ] && exit 1 || exit 0
