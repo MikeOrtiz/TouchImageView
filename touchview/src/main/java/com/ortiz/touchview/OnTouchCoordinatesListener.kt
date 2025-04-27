664d045c31f78c6e70df10272aae6a7473d36ad5
sonder-joker <highmagic@outlook.com>
2025-04-27 16:59:50 +0800

    support kotlin dsl for function call

--- touchview/src/main/java/com/ortiz/touchview/OnTouchCoordinatesListener.kt
+++ touchview/src/main/java/com/ortiz/touchview/OnTouchCoordinatesListener.kt
@@ -4,6 +4,6 @@ import android.graphics.PointF
 import android.view.MotionEvent
 import android.view.View
 
-interface OnTouchCoordinatesListener {
+fun interface OnTouchCoordinatesListener {
     fun onTouchCoordinate(view: View, event: MotionEvent, bitmapPoint: PointF)
 }
